05dc28fc432ca15bba27d8ff64f4a9fd0e3f0561
InnaKorsun <slvjova.inna@gmail.com>
2018-09-16 22:12:17 +0300

    Add task1 file

---
+++ task1.py
@@ -0,0 +1,18 @@
+#1) is number
+s = input()
+isnum = s.isdigit()
+print(isnum)
+#2)white space in str
+s = input()
+prob = s.count(" ")
+print(prob)
+#3)dot  in str
+s = input()
+dot = s.count(".")
+print(dot)
+#4)homework string 100
+s  = "Homework"
+print(s.center(100))
+#5) uppercase all case letters
+s = input()
+print(s.title())
