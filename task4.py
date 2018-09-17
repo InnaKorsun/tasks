ccad9290b1b0c7a95ae5400f468abb98203d269f
InnaKorsun <slvjova.inna@gmail.com>
2018-09-17 18:22:09 +0300

    Add 3-6 tasks file

---
+++ task4.py
@@ -0,0 +1,7 @@
+a = int(input("Enter a"))
+b = int(input("Enter b"))
+c = int(input("Enter c"))
+if a + b > c and a + c > b and b + c > a:
+    print("YES")
+else:
+    print("NO")
---
+++ task6.py
@@ -0,0 +1,9 @@
+a = int(input("Enter a"))
+b = int(input("Enter b"))
+c = int(input("Enter c"))
+if a==b and a==c:
+    print("3")
+elif a==b or a==c:
+    print("2")
+else:
+    print("0")
