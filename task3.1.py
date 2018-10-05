2c344aa3e3dfefb2bd9207de46183660e4d10c3e
InnaKorsun <slvjova.inna@gmail.com>
2018-10-05 22:49:46 +0300

    Update 3.1 file

--- task3.1.py
+++ task3.1.py
@@ -19,35 +19,3 @@ def numbers (*r):
     return r[1]
 numbers(*r)
 
-words =['aaa', 'be', 'abc', 'hello']
-#вывести количество строк у которых первый и последний литерал одинаковый
-def match_ends (words):
-    count = 0
-    for i in words:
-        if len(i)>=2 and i[0]==i[-1]:
-           count+=1
-    print(count)
-match_ends(words)
-
-#отсортировать лист:первыми идут слова на х потом в алфавтином порядке
-r = ['mix', 'xyz', 'apple', 'xanadu', 'aardvark']
-def front_x(r):
-   x_list = []
-   r = sorted(r)
-   for i in range(len(r)-1,-1,-1):
-       if r[i][0]=="x":
-           x_list.append(r[i])
-           r.pop(i)
-   x_list = sorted(x_list)
-   mix = x_list+r
-   print(mix)
-   return mix
-front_x(r)
-
-# отсортировать лист с тюплами  - тюплы по последнему значению
-t = [(1, 7), (1, 3), (3, 4, 5), (2, 2)]
-def sort_last(t):
-    d = sorted(t,key=lambda tup:tup[len(tup)-1])
-    print(d)
-sort_last(t)
-
