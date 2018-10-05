2558e94e4343bb5f6f99fc63471bd0d5f125be94
InnaKorsun <slvjova.inna@gmail.com>
2018-10-05 22:45:07 +0300

    Update 3.1 file

--- task3.1.py
+++ task3.1.py
@@ -1,31 +1,33 @@
 #песня ла ла ла
-def sing (a=3,b=3,c=0):
-    cup  = "-".join(["la"]*b)+'\n' #формируем ла ла строку с
-    text =cup*a # формируем a строк
+def sing(a=3, b=3, c=0):
+    cuplet  = "-".join(["la"]*b)+'\n' #формируем ла ла строку с
+    text =cuplet*a # формируем a строк
     text = text.strip()# удаляем последний перенос строки
     text = text+"." # добавляем точку
     if c == 1:
         text = text.replace(".","!") # если не точка - ставим знак восклицания
-    print(text)
+    #print(text)
     return text
 sing(4,6,0)
 
 #выводит второе по возрастанию значение из переданных аргументов.
-def numbers ():
-    l = [x for x in input("Enter sequence number by split whitespace:").split(" ")]
-    l = sorted(l)
-    print(l[1])
-numbers()
 
-l =['aaa', 'be', 'abc', 'hello']
+r = [x for x in input("Enter sequence number by split whitespace:").split()]
+def numbers (*r):
+    r = sorted(r)
+    print(r[1])
+    return r[1]
+numbers(*r)
+
+words =['aaa', 'be', 'abc', 'hello']
 #вывести количество строк у которых первый и последний литерал одинаковый
-def match_ends ():
+def match_ends (words):
     count = 0
-    for i in l:
+    for i in words:
         if len(i)>=2 and i[0]==i[-1]:
            count+=1
     print(count)
-match_ends()
+match_ends(words)
 
 #отсортировать лист:первыми идут слова на х потом в алфавтином порядке
 r = ['mix', 'xyz', 'apple', 'xanadu', 'aardvark']
